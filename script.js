d34ecc3f5605d2e9a14a62adabcda57c2b6b4a7d
Oscar-Peregrina <79479234+Oscar-Peregrina@users.noreply.github.com>
2021-02-22 13:59:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$("carouselExampleControls").carousel({
+    interval: 5000
+});
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
